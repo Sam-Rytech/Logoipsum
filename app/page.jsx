5d1f08ea4d7f3bbf1a16fab15334e42b11aa7d23
Omolayo Temitayo <temitayoomolayo@gmail.com>
2025-09-21 14:12:45 +0100

    Updated the import statements in the route page

--- app/page.jsx
+++ app/page.jsx
@@ -1,56 +1,29 @@
 "use client"
 import Navbar from '@/components/Navbar'
 import Hero from '@/components/Hero'
-import Companies from '@/components/Companies'
-import Commitments from '@/components/Commitments'
-import HowItWorks from '@/components/HowItWorks'
-import OurWork from '@/components/OurWork'
+import Services from '@/components/Services'
+import About from '@/components/About'
+import Projects from '@/components/Projects'
 import CTASection from '@/components/CTASection'
 import Team from '@/components/Team'
 import Testimonials from '@/components/Testimonials'
 import FAQ from '@/components/FAQ'
-import FinalCTA from '@/components/FinalCTA'
 import Footer from '@/components/Footer'
 
 
 export default function HomePage() {
   return (
-    <main className="flex flex-col min-h-screen">
-      {/* Navbar */}
+    <main className="text-gray-900 bg-white">
       <Navbar />
-
-      {/* Hero Section */}
       <Hero />
-
-      {/* Companies Section */}
-      <Companies />
-
-      {/* Commitments Section */}
-      <Commitments />
-
-      {/* How It Works Section */}
-      <HowItWorks />
-
-      {/* Our Work Section */}
-      <OurWork />
-
-      {/* CTA Section */}
-      <CTASection />
-
-      {/* Team Section */}
+      <Services />
+      <About />
       <Team />
-
-      {/* Testimonials Section */}
-      <Testimonials />
-
-      {/* FAQ Section */}
+      <Projects />
       <FAQ />
-
-      {/* Final CTA Section */}
-      <FinalCTA />
-
-      {/* Footer */}
+      <Testimonials />
+      <CTASection />
       <Footer />
     </main>
-  )
+  );
 }
